831a713213c528c36c8856ce7e01aeb6e2f943e4
286179-EugeneRussel <eugene.russel@usthealthproof.com>
2025-03-03 11:23:19 +0530

    correction

--- src/app/team-details/team-details.component.ts
+++ src/app/team-details/team-details.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrl: './team-details.component.css'
 })
 export class TeamDetailsComponent {
-  url: string ='https://angular.io'
+  url: string = 'https://dev.usthealthproofconnect.com/qliksense/single/?appid=4ee0d04a-525d-403b-9c3b-1f40d4fd5f0a&sheet=4fddf656-2dcd-44e9-9cde-4f4c92a29f16&theme=horizon&opt=ctxmenu,currsel';
   styleUrl;
  
   constructor(private sanitizer: DomSanitizer) {
